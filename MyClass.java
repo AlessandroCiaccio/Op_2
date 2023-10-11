e6ebaf110731e9de9d2f6ab66af46b7f44ce4fe5
Alessandro <sandroscro@libero.it>
2023-10-11 15:42:29 +0200

    Added Main

---
+++ MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String args[]) {
+      double x=12, y=6, q=4;
+      double a=(x+y+q)/3;
+
+      System.out.println("The average value is " + a);
+    }
+}
