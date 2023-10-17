027e2699de6760df252fc080b5f6b400b9fbc62d
Alessandro <sandroscro@libero.it>
2023-10-17 11:45:18 +0200

    Aggiunto metodo

--- MyClass.java
+++ MyClass.java
@@ -1,8 +1,11 @@
 public class MyClass {
     public static void main(String args[]) {
-      double x=12, y=6, q=4;
-      double a=(x+y+q)/3;
+      float x=12, y=6, z=4;
 
-      System.out.println("The average value is " + a);
+      System.out.println("The average value is " + media(x,y,z));
+    }
+    public static double media(float x, float y, float z){
+        float media=(x+y+z)/3;
+        return media;
     }
 }
